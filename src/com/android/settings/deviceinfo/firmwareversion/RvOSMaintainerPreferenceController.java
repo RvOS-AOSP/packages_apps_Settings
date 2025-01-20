26bd3b3c13f92861edb083dd228ef36ed965f860
Rve27 <rve27github@gmail.com>
2025-01-21 07:50:29 +0800

    Settings: Display RvOS Maintainer in device info
    Signed-off-by: Rve27 <rve27github@gmail.com>

---
+++ src/com/android/settings/deviceinfo/firmwareversion/RvOSMaintainerPreferenceController.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2024-2025 RvOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import android.text.TextUtils;
+
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class RvOSMaintainerPreferenceController extends BasePreferenceController {
+
+    private static final String TAG = "RvOSMaintainerCtrl";
+    private static final String RVOS_MAINTAINER = "ro.rvos.maintainer";
+
+    public RvOSMaintainerPreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        String mRvOSMaintainer = SystemProperties.get(RVOS_MAINTAINER,
+                this.mContext.getString(R.string.device_info_default));
+        return mRvOSMaintainer;
+    }
+}
